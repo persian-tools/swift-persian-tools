272b235c6b7317407a09341ed641872c9764e65e
Saeed Rezaei <saeed.rezaei.sa@gmail.com>
2021-06-12 21:36:05 +0430

    feat: Verify Card Number (#2)
    feat: implement verify card number feature

--- SwiftPersianTools/String.swift
+++ SwiftPersianTools/String.swift
@@ -68,4 +68,28 @@ public extension String {
 
         return sum < 2 ? check == sum : check + sum == 11
     }
+
+    func verifyCardNumber() -> Bool {
+        if self.count < 16 {
+            return false
+        }
+
+        var radix = 0
+        var subDigit = 0
+        var sum = 0
+        var index = 0
+
+        for number in self {
+            radix = index % 2 == 0 ? 2: 1
+            guard let intNumber = Int("\(number)") else {
+                return false
+            }
+            subDigit = intNumber * radix
+            sum += subDigit > 9 ? subDigit - 9: subDigit
+
+            index += 1
+        }
+
+        return sum % 10 == 0
+    }
 }
