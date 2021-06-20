a18a5167395656f6e15e924caee3d70b79879f84
Saeed <saeed.rezaei.sa@gmail.com>
2021-06-20 15:57:00 +0430

    feat: validate iranian Sheba

--- SwiftPersianTools/Extensions/String/Sheba.swift
+++ SwiftPersianTools/Extensions/String/Sheba.swift
@@ -5,47 +5,27 @@
 import Foundation
 
 public extension String {
-    func validate() -> Bool {
+    func validateIranianSheba() -> Bool {
         if self.count != 26 {
             return false
         }
 
-        let shebaRegEx = "/IR[0-9]{2}([0-9]{3})[0-9]{19}/"
+        let shebaRegEx = "IR[0-9]{2}([0-9]{3})[0-9]{19}"
         let testCase = NSPredicate(format: "SELF MATCHES %@", shebaRegEx)
         if !testCase.evaluate(with: self) {
             return false
         }
 
-        let code: [Character] = self.suffix(2)
-        guard let firstChar = Int("\(code[0].utf8)") else {
-            return false
+        var a = self.utf8.map{ $0 }
+        while a.count < 4 {
+            a.append(0)
+        }
+        let b = a[4..<a.count] + a[0..<4]
+        let remainder = b.reduce(0) { (r, u) -> Int in
+            let i = Int(u)
+            return i > 64 ? (100 * r + i - 55) % 97: (10 * r + i - 48) % 97
         }
 
-        guard let secondChar = Int("\(code[1].utf8)") else {
-            return false
-        }
-
-        let d1 = firstChar - 65 + 10
-        let d2 =  secondChar - 65 + 10
-
-        var shebaCode = self[self.index(self.startIndex, offsetBy: 4)...]
-        let afterCodeIndex = self.index(self.startIndex, offsetBy: 2)
-
-        let d3 = self[afterCodeIndex..<self.index(afterCodeIndex, offsetBy: 2)]
-
-        shebaCode = "\(d1)\(d2)\(d3)\(shebaCode)"
-
-        return false
-    }
-
-    fileprivate func iso7064Mod97_10(iban: String)-> Int {
-        var remainder = iban
-        var block: String
-
-        while remainder.count > 2 {
-            block = remainder[remainder.startIndex...remainder.index(remainder.startIndex, offsetBy: 9)]
-//            remainder = (Int(block) % 97) + remainder.suffix(block.count)
-        }
-
+        return remainder == 1
     }
 }
