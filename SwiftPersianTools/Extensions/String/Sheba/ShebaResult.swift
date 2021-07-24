20e292e3bbfe62aa57977d90891cde79944bf170
Saeedrz <saeed.rezaei.sa@gmail.com>
2021-07-24 02:48:57 -0700

    feat(recognize bank sheba): implement recognize bank info from sheba

---
+++ SwiftPersianTools/Extensions/String/Sheba/ShebaResult.swift
@@ -0,0 +1,19 @@
+//
+//  ShebaResult.swift
+//  SwiftPersianTools
+//
+//  Created by Saeed on 7/23/21.
+//
+
+import Foundation
+
+public struct ShebaResult {
+    public var name: String
+    public var code: String
+    public var nickname: String
+    public var persianName: String
+    public var accountNumber: String?
+    public var accountNumberAvailable: Bool
+    public var formattedAccountNumber: String?
+}
+
--- SwiftPersianToolsTests/TestStringExtension.swift
+++ SwiftPersianToolsTests/TestStringExtension.swift
@@ -111,4 +111,9 @@ class TestStringExtension: XCTestCase {
         XCTAssertEqual("۰۱۲۳٤٥٦۷۸۹".digitsArtoEn(), "0123456789")
         XCTAssertEqual("۸۹123٤٥".digitsArtoEn(), "8912345")
     }
+
+    func testRecognizeBankFromSheba() throws {
+        let sheba = "IR820540102680020817909002"
+        XCTAssertTrue(sheba.recognizeBankInfo()?.code == "054")
+    }
 }
