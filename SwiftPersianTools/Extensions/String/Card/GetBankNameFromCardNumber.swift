ef1cd9dd785ceb13c75db6f28ee60cb730fb8bfc
Saeed <saeed.rezaei.sa@gmail.com>
2021-07-26 15:46:36 +0430

    feat: implement get Bank Name From Card Number

---
+++ SwiftPersianTools/Extensions/String/Card/GetBankNameFromCardNumber.swift
@@ -0,0 +1,15 @@
+//
+//  GetBankNameFromCardNumber.swift
+//  SwiftPersianTools
+//
+//  Created by Saeed on 7/25/21.
+//
+
+import Foundation
+
+public extension String {
+    func getBankNameFromCardNumber() -> String {
+
+        return ""
+    }
+}
